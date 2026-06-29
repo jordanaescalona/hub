febb3999a381eb3de38da939f2d3b936fab6c1f7
jordanaescalona <jordana.escalona@gmail.com>
2026-06-29 18:19:18 -0300

    move portfolio to subfolder

---
+++ portfolio/js/main.js
@@ -0,0 +1,11 @@
+const API = 'https://hub-api.jordana-escalona.workers.dev';
+
+// Navbar scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.boxShadow = '0 2px 20px rgba(0,0,0,0.08)';
+    } else {
+        navbar.style.boxShadow = 'none';
+    }
+});
